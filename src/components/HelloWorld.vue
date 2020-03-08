4cfc6bcca6863cab82ba8ac1d954c3e35b84371d
Antoine ROBERT <tibetoine@gmail.com>
2020-03-08 01:55:03 +0100

    Test pour CircleCi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,10 +88,6 @@ export default {
   data: () => ({
     ecosystem: [
       {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader'
-      },
-      {
         text: 'github',
         href: 'https://github.com/vuetifyjs/vuetify'
       },
